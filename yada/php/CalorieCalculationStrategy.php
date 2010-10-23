e5d9ccda80e784d4cc57b98dd2258d8984118f28
axs1430 <axs1430@rit.edu>
2010-10-22 20:59:04 -0400

    stttt

--- yada/php/CalorieCalculationStrategy.php
+++ yada/php/CalorieCalculationStrategy.php
@@ -3,7 +3,7 @@ require_once('config.php');
 
 interface CalorieCalculationStrategy {
 
-    public function calculateCalories(CalorieMetrics $calMetrics);
+    public function calculateNutrition(CalorieMetrics $calMetrics);
 }
 
 ?>
